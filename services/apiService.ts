661ca4a2fd2343779e8ef7f2355fff6fe6792d03
Henryarrovin <henry.a.s.arrovin@gmail.com>
2025-04-06 04:36:08 +0530

    Added environment variables.

--- services/apiService.ts
+++ services/apiService.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { API_BASE_URL } from "@env";
 
-const API_BASE_URL = "http://192.168.0.106:8080/api";
+// const API_BASE_URL = "http://192.168.0.106:8080/api";
 // const API_BASE_URL = "http://192.168.137.178:8080/api";
 
 const apiService = axios.create({
